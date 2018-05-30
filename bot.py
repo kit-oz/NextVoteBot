cb373116c9344dbf4ec95e3054335a25ad371476
Nikita B <nikita.balobanov@gmail.com>
2018-05-30 14:53:49 +0500

    Added logging

--- bot.py
+++ bot.py
@@ -1,36 +1,25 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import os
-import sys
-from threading import Thread
 
 from telegram.ext import Updater
-from telegram.ext import CommandHandler
 
 from config import BOT_TOKEN, HOST_NAME, PORT_NUMBER
 from poll import init_handlers
-from wrappers import admin_only
 
 
 def main():
     logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                         level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    def error(bot, update, error):
+        logger.warning('Update "%s" caused error "%s"', update, error)
 
     updater = Updater(token=BOT_TOKEN)
-
-    def stop_and_restart():
-        """Gracefully stop the Updater and replace the current process with a new one"""
-        updater.stop()
-        os.execl(sys.executable, sys.executable, *sys.argv)
-
-    @admin_only
-    def restart(bot, update):
-        update.message.reply_text('Bot is restarting...')
-        Thread(target=stop_and_restart).start()
-
-    updater.dispatcher.add_handler(CommandHandler('restart', restart))
-    init_handlers(updater.dispatcher)
+    dp = updater.dispatcher
+    init_handlers(dp)
+    dp.add_error_handler(error)
 
     # updater.start_polling()
     updater.start_webhook(listen="0.0.0.0",
